154a3122ba86a8091b9135d7e6dbde88205fa7e0
tkuyyt <133862639+tkuyyt@users.noreply.github.com>
2023-06-02 15:48:21 +0800

    shap_app

---
+++ shop_app/service-worker.js
@@ -0,0 +1,48 @@
+var dataCacheName = 'shop_app_v1';
+var cacheName = 'shop_app';
+var filesToCache = [
+  '/shop_app/',
+  '/shop_app/index.html',
+  '/shop_app/chart.js',
+  '/shop_app/style.css',
+  '/shop_app/battery.png',
+  '/shop_app/bicycle.png',
+  '/shop_app/motocross_bike.png',
+  '/shop_app/police_motorbike.png',
+
+];
+
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('activate', function(e) {
+  console.log('[ServiceWorker] Activate');
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (key !== cacheName && key !== dataCacheName) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          caches.delete(key);
+        }
+      }));
+    })
+  );
+  /*
+   * Fixes a corner case in which the app wasn't returning the latest data.
+   * You can reproduce the corner case by commenting out the line below and
+   * then doing the following steps: 1) load app for first time so that the
+   * initial New York City data is shown 2) press the refresh button on the
+   * app 3) go offline 4) reload the app. You expect to see the newer NYC
+   * data, but you actually see the initial data. This happens because the
+   * service worker is not yet activated. The code below essentially lets
+   * you activate the service worker faster.
+   */
+  return self.clients.claim();
+});
